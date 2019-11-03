1622cb854daa55d6c3e6098bc2c2210ad4edf8b9
= <=>
2019-11-03 11:42:20 -0500

    Fixed routing to profile

--- client/src/components/DashboardComponents/UserNav.js
+++ client/src/components/DashboardComponents/UserNav.js
@@ -29,7 +29,7 @@ export default class UserNav extends Component {
 
     friendRequests = event => {
         console.log("friend requests clicked")
-        this.setState({redirect: '/friend-requests'})
+        this.setState({ redirect: '/friend-requests' })
     }
 
     addFriends = event => {
@@ -40,61 +40,70 @@ export default class UserNav extends Component {
 
     editProfile = event => {
         console.log("edit profile clicked")
-        this.setState({redirect: "/profile"})
+        this.setState({ redirect: "/profile" })
+        return < Redirect
+            to={{
+                pathname: '/profile',
+                state: {
+                    user_id: 1,
+                    profile_id: 1,
+                }
+            }
+            } />
     }
-    
+
     render() {
         if (this.state.redirect === "/friend-requests") {
             return (
-            <Redirect to="/profile"/>
+                <Redirect to="/friend-requests" />
             );
-        } else if (this.state.redirect === "profile") {
+        } else if (this.state.redirect === "/profile") {
             return (
                 <Redirect
-                        to={{
-                            pathname: '/profile',
-                            state: {
-                                user_id: 0,
-                                profile_id: 0,
+                    to={{
+                        pathname: '/profile',
+                        state: {
+                            user_id: 0,
+                            profile_id: 0,
                         }
-                 }}/>
+                    }} />
             )
-        }   
+        }
         // add else to check to check final redirect
 
-        return(
-           
-                <div className="shadow">
-                
-            <div className="card">
-            <div className="card-body">
-                <div>
-                    <img className="rounded-circle img-responsive" width="65" height="65" src={this.user.picture} alt="" />
+        return (
+
+            <div className="shadow">
+
+                <div className="card">
+                    <div className="card-body">
+                        <div>
+                            <img className="rounded-circle img-responsive" width="65" height="65" src={this.user.picture} alt="" />
+                        </div>
+                        <div className="h5">{this.user.name} </div>
+                        <div className="h7 text-muted">@{this.user.username}</div>
+                        <div className="h7">{this.user.bio}</div>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item align-items-center">
+                            <button className="btn btn-link" onClick={this.addFriends}> Add Friends </button>
+
+                        </li>
+                        <li className="list-group-item align-items-center">
+                            <button className="btn btn-link" onClick={this.friendRequests}> Friend Requests</button>
+
+                        </li>
+                        <li className="list-group-item align-items-center">
+                            <button className="btn btn-link" onClick={this.editProfile}> Edit Profile</button>
+
+                        </li>
+                    </ul>
                 </div>
-                <div className="h5">{this.user.name} </div>
-                <div className="h7 text-muted">@{this.user.username}</div>
-                <div className="h7">{this.user.bio}</div>
+
             </div>
-            <ul className="list-group list-group-flush">
-            <li className="list-group-item align-items-center">
-                <button className="btn btn-link" onClick={this.addFriends}> Add Friends </button>                
-                
-            </li>
-            <li className="list-group-item align-items-center">
-                <button className="btn btn-link" onClick={this.friendRequests}> Friend Requests</button>
-    
-            </li>
-            <li className="list-group-item align-items-center">
-                <button className="btn btn-link" onClick={this.editProfile}> Edit Profile</button>
-               
-            </li>
-        </ul>
-        </div>
 
-        </div>
-            
 
-            
+
         );
     }
 }
