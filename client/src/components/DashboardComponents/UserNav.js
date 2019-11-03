6503d15fb2f8c8e4b269de4be5bcc8ac7ed0cb00
Sonia Zaldana Calles <zaldanasonia@gmail.com>
2019-11-03 11:57:12 -0400

    Redirecting from dashboard

--- client/src/components/DashboardComponents/UserNav.js
+++ client/src/components/DashboardComponents/UserNav.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import "../../css/UserDashboard.css"
 import User1 from "./DashboardAssets/User1.jpg"
+import { Redirect } from "react-router";
+import { Link } from "react-router-dom"
+
 
 
 export default class UserNav extends Component {
@@ -20,11 +23,42 @@ export default class UserNav extends Component {
             bio: "I'm so tired",
             time: "10"
         }
+        this.state = {
+            redirect: ""
+        }
+    }
+
+    friendRequests = event => {
+        console.log("friend requests clicked")
+        this.setState({redirect: '/friend-requests'})
+    }
+
+    addFriends = event => {
+        console.log("add friends clicked")
+        // set it to your path
+        // this.setState({redirect: '/friend-requests'})
+    }
+
+    editProfile = event => {
+        console.log("edit profile clicked")
+        // set it to your path
+        // this.setState({redirect: '/friend-requests'})
     }
     
+    
     render() {
+
+        if (this.state.redirect === "/friend-requests") {
+            return (
+            <Redirect to="/friend-requests"/>
+            );
+        }  
+
+        // add elses to check fo redirect
+
         return(
-            <div className="shadow">
+           
+                <div className="shadow">
                 
             <div className="card">
             <div className="card-body">
@@ -37,21 +71,24 @@ export default class UserNav extends Component {
             </div>
             <ul className="list-group list-group-flush">
             <li className="list-group-item align-items-center">
-                <button className="btn btn-link"> Add Friends </button>                
+                <button className="btn btn-link" onClick={this.addFriends}> Add Friends </button>                
                 
             </li>
             <li className="list-group-item align-items-center">
-                <button className="btn btn-link"> Friend Requests</button>
+                <button className="btn btn-link" onClick={this.friendRequests}> Friend Requests</button>
     
             </li>
             <li className="list-group-item align-items-center">
-                <button className="btn btn-link"> Settings</button>
+                <button className="btn btn-link" onClick={this.editProfile}> Edit Profile</button>
                
             </li>
         </ul>
         </div>
 
         </div>
+            
+
+            
         );
     }
 }
