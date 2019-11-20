05ef91ac6d87e32015183c9470d0be8a98586ec7
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-11-19 20:25:04 -0500

    MongoDB Connected

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("config");
+const passport = require("passport");
+
+//bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//connect to mongo db
+const db = config.get("mongoURI");
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
