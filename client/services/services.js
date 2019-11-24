a7124487ba904bd45b5e72103ac8699dd6f43da8
Sonia Zaldana Calles <zaldanasonia@gmail.com>
2019-11-24 18:26:12 -0500

    Services

--- client/services/services.js
+++ client/services/services.js
@@ -1,2 +1,16 @@
 const axios = require('axios')
 
+var instance = axios.create({
+    baseURL: 'https://localhost:5000/api/',
+    timeout: 1000,
+    headers: {'Authorization': 'TOKEN SHOULD BE HERE'}
+  });
+
+// Get checkins for given user id 
+const getCheckinsForUser = async () => {
+    try {
+      return await axios.get('checkins')
+    } catch (error) {
+      console.error(error)
+    }
+  }
