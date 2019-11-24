6e60768989e06d3995423c5380ed6e79dccc81d6
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-11-24 16:16:06 -0500

    sign token has isAdmin as payload too now

--- utils/jwtSign.js
+++ utils/jwtSign.js
@@ -2,7 +2,11 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function createToken(user) {
-  return jwt.sign({ id: user.id, name: user.name }, config.get("jwt_secret"), {
-    expiresIn: 3600
-  });
+  return jwt.sign(
+    { id: user.id, name: user.name, isAdmin: user.isAdmin },
+    config.get("jwt_secret"),
+    {
+      expiresIn: 3600
+    }
+  );
 };
