af066bf4d0c521960dcd4de3df75102d88dd6486
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-11-24 18:16:23 -0500

    Login lOGIC ADDED to frontend

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,13 +1,17 @@
-export const login = (id) => {
-    console.log("TRIGGERED ACTION LOGIN");
-    return {
-        type: "LOGIN",
-        payload: id
-    };
+import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+export const loginUser = id => {
+  console.log("TRIGGERED ACTION LOGIN");
+  return {
+    type: "LOGIN",
+    payload: id
+  };
 };
 
 export const logoff = () => {
-    return {
-        type: "LOGOFF"
-    };
+  return {
+    type: "LOGOFF"
+  };
 };
