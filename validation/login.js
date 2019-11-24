76e8e3b012dc382fc0cdbe7a9ca32f4b1b9761c5
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-11-24 18:24:48 -0500

    Login request to api added

--- validation/login.js
+++ validation/login.js
@@ -5,14 +5,11 @@ module.exports = function validateLoginInput(data) {
   let errors = {};
 
   //if no input make them empty strings because the Validator library only takes in strings as input
-  data.email = !isEmpty(data.email) ? data.email : "";
+  data.username = !isEmpty(data.username) ? data.username : "";
   data.password = !isEmpty(data.password) ? data.password : "";
 
-  if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid";
-  }
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
+  if (Validator.isEmpty(data.username)) {
+    errors.username = "username field is required";
   }
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
