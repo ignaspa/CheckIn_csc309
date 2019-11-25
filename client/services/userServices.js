a47a241e612680ef349a9eab5d4ef9bbbee8a6c5
ignaspa <ignaspan@gmail.com>
2019-11-24 19:36:13 -0500

    user axios calls

--- client/services/userServices.js
+++ client/services/userServices.js
@@ -8,6 +8,40 @@ const getUserObject = async () => {
         return res;
     })
     .catch(err => {
+        console.log(err);
+        return null;
+    });
+}
+const getAllUserObjects = async () => {
+    axios
+    .get('https://localhost:5000/api/users/all')
+    .then(res => {
+        return res;
+    })
+    .catch(err => {
+        console.log(err);
+        return null;
+    });
+}
+const changeUserDetails = async (name, bio) => {
+    axios
+    .patch('https://localhost:5000/api/users/details', {newbio: bio, newname: name})
+    .then(res => {
+        return res;
+    })
+    .catch(err => {
+        console.log(err);
+        return null;
+    });
+}
+const changeUserImage = async (newimage) => {
+    axios
+    .patch('https://localhost:5000/api/users/profilepic', {newpic: newimage})
+    .then(res => {
+        return res;
+    })
+    .catch(err => {
+        console.log(err);
         return null;
     });
 }
