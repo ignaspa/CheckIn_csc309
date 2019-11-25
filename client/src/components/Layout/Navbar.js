033d15a672ecd51f0f17011ddb07b473acf9d81b
Marco Angeli <marco.angeli@mail.utoronto.ca>
2019-11-24 23:45:17 -0500

    Added more functionality to NavBar

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logoutUser } from '../../redux/actions.js';
+import { bindActionCreators } from "redux";
 
-
-export default class Navbar extends Component {
+class Navbar extends Component {
     logOut = (event) => {
     }
   render() {
+      const logOutButton = this.props.user.isAuthenticated ? (<ul className="navbar-nav mr-auto list-group-horizontal">
+          <li>
+              <Link to={"/"} className="nav-link text-white navitem" onClick={this.logOut}>
+                  Logout
+              </Link>
+          </li>            
+      </ul>) : "";
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -16,16 +25,21 @@ export default class Navbar extends Component {
               </Link>
             </li>            
           </ul>
-        <ul className="navbar-nav mr-auto list-group-horizontal">
-            <li>
-                <Link to={"/"} className="nav-link text-white navitem" onClick={this.logOut}>
-                    Logout
-                </Link>
-            </li>            
-        </ul>
+          {logOutButton}
+        
         </nav>
       </div>
     );
   }
 }
 
+const mapStateToProps = store => ({
+    user: store.user,
+    errors: store.errors,
+    userData: store.userData
+});
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({ logoutUser: logoutUser }, dispatch);
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
