0777daf83160ba0b72a34dd4a84dac3a0ddc8009
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-11-26 16:59:39 -0500

    Checkin Model time attribute type changed to Date. Create Checkin route time removed because it is set by default to Date.now() in the model

--- routes/api/checkins.js
+++ routes/api/checkins.js
@@ -21,17 +21,16 @@ router.get(
   }
 );
 
-// Route to get active checkin for this user 
+// Route to get active checkin for this user
 router.get(
   "/active",
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     try {
-      const user = await User.findById(req.user.id)
-                  .catch(err => {
-                    res.status(400).json(err)
-                  })            
-    res.json(user.activeCheckin)
+      const user = await User.findById(req.user.id).catch(err => {
+        res.status(400).json(err);
+      });
+      res.json(user.activeCheckin);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -44,25 +43,26 @@ router.get(
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     try {
-      const user = await User.findById(req.user.id)
-                  .catch(err => {
-                    res.status(400).json(err)
-                  })    
-                  
-      let userList = user.friends
-      userList.push(req.user.id)
-      
-      let query = Checkin.where("userid").in(userList)
+      const user = await User.findById(req.user.id).catch(err => {
+        res.status(400).json(err);
+      });
+
+      let userList = user.friends;
+      userList.push(req.user.id);
+
+      let query = Checkin.where("userid")
+        .in(userList)
         .sort({ time: -1 });
 
-        let promise = query.exec()
+      let promise = query.exec();
 
-        promise.then(response => {
-          res.json(response)
-        }).catch(err => {
-          res.status(400).json(err)
+      promise
+        .then(response => {
+          res.json(response);
         })
-
+        .catch(err => {
+          res.status(400).json(err);
+        });
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -85,8 +85,8 @@ router.get(
 
 // Create one checkin
 // {
-//   action: <action>, 
-//   location: <location>, 
+//   action: <action>,
+//   location: <location>,
 //   message: <message>
 // }
 router.post(
@@ -96,17 +96,17 @@ router.post(
     const checkin = new Checkin({
       action: req.body.action,
       location: req.body.location,
-      time: req.body.time,
       message: req.body.message,
       userid: req.user.id
     });
 
     try {
       const newCheckin = await checkin.save();
-      User.findByIdAndUpdate(req.user, 
-        {$inc : {totalCheckins : 1}},
+      User.findByIdAndUpdate(
+        req.user,
+        { $inc: { totalCheckins: 1 } },
         { activeCheckin: newCheckin.id }
-      ).catch((err) => {
+      ).catch(err => {
         res.status(400).json({ message: err.message });
       });
 
@@ -127,26 +127,25 @@ router.delete(
   "/",
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
-    
-    let removed = await Checkin.findOneAndDelete({id: req.body.checkinId }
-      ).catch((err) => {
-        res.status(400).json({ message: err.message });
-      }
-    );
-    
-    let user = await User.find({id: removed.userId})
-                .catch((error) => {
-                  res.status(400).json({message: error})
-                })
+    let removed = await Checkin.findOneAndDelete({
+      id: req.body.checkinId
+    }).catch(err => {
+      res.status(400).json({ message: err.message });
+    });
+
+    let user = await User.find({ id: removed.userId }).catch(error => {
+      res.status(400).json({ message: error });
+    });
 
     // if update being deleted is the active one
     if (user.activeCheckin == req.body.checkinId) {
-      User.findAndUpdate({id: removed.userId},
-        {$inc : {totalCheckins : -1}},
-        { activeCheckin: null}
-      ).catch((err) => {
+      User.findAndUpdate(
+        { id: removed.userId },
+        { $inc: { totalCheckins: -1 } },
+        { activeCheckin: null }
+      ).catch(err => {
         res.status(400).json({ message: err.message });
-      });  
+      });
     }
     res.json({ message: "Deleted checkin", checkin: removed });
   }
