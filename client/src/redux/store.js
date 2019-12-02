3fffcd84ed6b8cd12bcbbf1e9d73733ae73dd8c2
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-12-01 23:34:47 -0500

    Redux Devtools removed

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -6,8 +6,8 @@ const middleware = [thunk];
 const store = createStore(
   allReducers,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 export default store;
